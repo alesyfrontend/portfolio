39938372e73dbb65c603ec35a06f14f995b8adb0
alesyfrontend <alesy2007@gmail.com>
2024-07-14 15:04:17 -0300

    php

--- enviar.php
+++ enviar.php
@@ -1,21 +1,24 @@
 <?php
-    $nome = addslashes($_POST['nome']);
-    $nome = addslashes($_POST['email']);
-    $nome = addslashes($_POST['telefone']);
+$nome = addslashes($_POST["nome"]);
+$email = addslashes($_POST ["email"]);
+$telefone = addslashes($_POST ["telefone"]);
+
+$para = "alesy2007@gmail.com";
+$assunto = "Coleta de dados - Portfólio";
 
 
-    $para ="alesy2007@gmail.com";
-    $assunto = "coleta de dados - Portfólio";
+$corpo = "Nome:" .$nome. "\n". "E-mail:  ".$email. "\n". "Telefone: ".$telefone;
 
 
-    $corpo="Nome: ".$nome. "\n"."E-mail: ".$email."\n" ."Telefone: " .$telefone;
+$cabeça: "From: alesy2007@gmail.com" ."\n". "Replay-to:" .$email. "\n". "X-Mailer: PHP/" .phpversion();
 
-    $cabeca= "from :alesy@gmail.com" ."\n" ."Replay-to: ".$email. "\n"."X-Mailer:PHP/".phpversion();
-    
-    if (mail($para,$assunto,$corpo,$cabeca)){
-        echo("E-mail enviado com sucesso!");
-    } 
-        else{
-        echo("Houve um erro ao enviar o email");
-    }
-?>
+if  (mail ($para,$assunto,$corpo,$cabeça )){
+
+    echo ("E-mail enviado com sucesso!");
+} else{
+    echo ("Houve um erro ao enviar o email!");
+}
+
+
+
+?>  
